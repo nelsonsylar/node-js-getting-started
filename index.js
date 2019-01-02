86f0db0fdcb6e422e15ddb0f2a7962aeaa7386b7
sylar <a2135825@gmail.com>
2019-01-02 14:38:14 +0800

    init

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 const app=express()
 
-
-app.get('/hi', (req, res) => res.render('index.html'))
+app.use(app.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.get('/hi', (req, res) => res.render('hi'))
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
