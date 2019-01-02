8b66e20d4f7525ad8da5ee0d1896a6d0e9027328
sylar <a2135825@gmail.com>
2019-01-02 15:37:32 +0800

    enable to work as a demo

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
-  .use(express.static('public')
+  .use(express.static('public'))
   .get('/', (req, res) => res.sendfile(path.join(__dirname,'./index.html')))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
